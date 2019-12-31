7be2a71b8585e2bdbb0aec8446da6265aa96af2e
Danny Gleason <danny.c.gleason@gmail.com>
2019-12-31 12:35:17 -0500

    Updates

---
+++ precache-manifest.b543d2145838276603d2f3f844b7ce75.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4020221c8f413d85d15d39ad781e1509",
+    "url": "/portfolio-react-temp/index.html"
+  },
+  {
+    "revision": "2b4114e01265eaa51ac7",
+    "url": "/portfolio-react-temp/static/css/2.0decb1de.chunk.css"
+  },
+  {
+    "revision": "6cfae01f4d0e478fd995",
+    "url": "/portfolio-react-temp/static/css/main.2a746dfb.chunk.css"
+  },
+  {
+    "revision": "2b4114e01265eaa51ac7",
+    "url": "/portfolio-react-temp/static/js/2.a33ff887.chunk.js"
+  },
+  {
+    "revision": "5a06048307bab107be3a70912babd370",
+    "url": "/portfolio-react-temp/static/js/2.a33ff887.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6cfae01f4d0e478fd995",
+    "url": "/portfolio-react-temp/static/js/main.cf7902ce.chunk.js"
+  },
+  {
+    "revision": "5e9dc2c6340906e16e3a",
+    "url": "/portfolio-react-temp/static/js/runtime-main.d3c8e9a1.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/portfolio-react-temp/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/portfolio-react-temp/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/portfolio-react-temp/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/portfolio-react-temp/static/media/slick.f97e3bbf.svg"
+  }
+]);
